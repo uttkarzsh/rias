bd93d6fa60838202c9ba0503982d31e074168d29
Uttkarsh Pawan <utkbest@gmail.com>
2026-07-23 16:04:21 +0530

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-#![no_std]
-#![no_main]
+#![no_std] // no rust standard library
+#![no_main] // no main entry point
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    // call this on panic (original panic is in std lib)
     loop {}
 }
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // actual entry point as main does not exist here
     loop {}
 }
