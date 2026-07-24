ee48e9460dc522eb75a01ec603fb21398eaa1431
Uttkarsh Pawan <utkbest@gmail.com>
2026-07-24 17:49:24 +0530

    implement print macros

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,15 @@ use core::panic::PanicInfo;
 use crate::vga_buffer::print_smth;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     // call this on panic (original panic is in std lib)
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[unsafe(no_mangle)] // do not mangle the name of this fn
 pub extern "C" fn _start() -> ! {
-    print_smth();
-
+    println!("hello world");
+    panic!("wowowowoow");
     loop {}
 }
