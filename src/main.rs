8b5803695cd5fd715003a259008f85152926d58b
Uttkarsh Pawan <utkbest@gmail.com>
2026-08-15 02:35:28 +0530

    add double fault handler

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,16 @@ pub extern "C" fn _start() -> ! {
 
     rias::init();
 
+<<<<<<< HEAD
     // x86_64::instructions::interrupts::int3();
+=======
+    // x86_64::instructions::interrupts::int3();    // invoke a breakpoint interruption
+
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
+>>>>>>> b8b8078 (add double fault handler)
 
     #[cfg(test)]
     test_main();
